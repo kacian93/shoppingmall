53cfb1853e64232fe8e4c51f8df8af51edcf9657
park <kacian93@gmail.com>
2022-08-29 17:01:56 +0900

    Initial Commit

---
+++ DeliveryApp/DeliveryAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  DeliveryAppApp.swift
+//  DeliveryApp
+//
+//  Created by PC156 on 2022/08/29.
+//
+
+import SwiftUI
+
+@main
+struct DeliveryAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
